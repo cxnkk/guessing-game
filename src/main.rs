9cb0d128af7b6bbeca342ae9ffd6c5f45bffb80f
cxnkk <cenkoezkartal@gmail.com>
2025-04-11 15:28:17 +0200

    guess the number game

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,29 @@
+use std::{cmp::Ordering, io};
+use rand::Rng;
+
 fn main() {
-    let x = 42;
-    println!("Der Wert der Variable x ist {}", x);
-    let x = 11; // Shadowing
-    println!("Der Wert der Variable x ist {}", x);
-    let mut y = 97; // Mutable
-    println!("Der Wert der Variable y ist {}", y);
-    y = 3456;
-    println!("Der Wert der Variable y ist {}", y);
+    let correct = rand::rng().random_range(1..=100);
+    println!("Correct: {correct}");
+    println!("Hey, guess a number 1-100:");
     
-    const LEVEL:i32 = -88865; // Integer, positive sowie negative Zahlen möglich mit Type i
-    println!("Das Level beträgt {}", LEVEL);
-    const LEVEL2:u32 = 12345; // Unsigned Integer, nur positive Zahlen möglich mit Type u
-    println!("Das Level beträgt {}", LEVEL2);
-    let geschwindigkeit = 1.5; // Floating point numbers
-    println!("Deine Geschwindigkeit beträgt {} km/h", geschwindigkeit);
-    let booleans = true; // True oder false
-    println!("Dieser Boolean ist {}", booleans);
-    let character:char = 'a'; // Character, nur ein einzelnder Buchstabe möglich
-    println!("Hier wird der Buchstabe {} angezeigt", character);
+    loop {
+     let mut guess = String::new();
+    io::stdin()
+    .read_line(&mut guess)
+    .expect("Error reading input");
 
-    // komplexe Datentypen
-    let s:String = String::from("Hallo Welt!");
-    println!("Hi: {}", s);
-
-    let a:[char;4] = ['b','c','d','e'];
-    println!("Array demo: {}", a[0]);
+    let guess: u32 = match guess.trim().parse(){
+        Ok(num) => num,
+        Err(e) => {
+            println!("Error with parse, try again. {e}");
+            continue;
+        }
+    };
+    
+    match guess.cmp(&correct){
+        Ordering::Greater => println!("WRONG! You guessed to high."),
+        Ordering::Less => println!("WRONG! You guessed to low."),
+        Ordering::Equal => {println!("CONGRATS! You guessed correct."); break;},
+    };    
+    }
 }
